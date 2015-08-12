5f8210353b01fad0860d848c7c40a4f08b93372d
Mateus Cerqueira <mateuscg@ciandt.com>
2015-08-12 10:34:08 -0300

    jdModal directive changes to use templateUrl and new pattern.

--- modal.js
+++ modal.js
@@ -3,59 +3,85 @@
 define(['angular'], function () {
 
     angular.module('jedi.layout.modal', []).constant('jedi.layout.modal.ModalConfig', {
-        headerTpl: '<div class="panel-heading modal-header">{{titleTpl}}{{closeBtnTpl}}</div>',
-        closeBtnTpl: '<span class="glyphicon glyphicon-remove close"></span>',
-        titleTpl: '<strong><span class="glyphicon glyphicon-th"></span><jd-i18n>{{jdTitle}}</jd-i18n></strong>',
-        defaultElementClass: 'modal-body form-horizontal',
         defaultFormClass: 'form-validation',
-        defaultTableClass: 'table-dynamic'
-    }).directive('jdModal', ['jedi.layout.modal.ModalConfig', '$interpolate', '$timeout', function (ModalConfig, $interpolate, $timeout) {
+        defaultTableClass: 'table-dynamic',
+        defaultTemplateUrl: 'assets/libs/ng-jedi-layout/modal.html'
+    }).directive('jdModal', ['jedi.layout.modal.ModalConfig', '$timeout', '$compile', function (ModalConfig, $timeout, $compile) {
         return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                element.before($interpolate(ModalConfig.headerTpl)(angular.extend({
-                    closeBtnTpl: (!attrs.jdHideCloseBtn && attrs.jdHideCloseBtn != '' ? $interpolate(ModalConfig.closeBtnTpl)(angular.extend({}, attrs, scope)) : undefined),
-                    titleTpl: (attrs.jdTitle ? $interpolate(ModalConfig.titleTpl)(angular.extend({}, attrs, scope)) : undefined)
-                }, attrs, scope)));
-
-                element.addClass(ModalConfig.defaultElementClass);
-
-                if (element.is('form')) {
-                    element.addClass(ModalConfig.defaultFormClass);
-                    if (!attrs.name && attrs.jdTitle) {
-                        element.attr('name', attrs.jdTitle);
+            restrict: "A",
+            priority: 1000.1,
+            compile: function compile(cElement, cAttrs, cTransclude) {
+                if (cElement.is('form')) {
+                    cElement.addClass(ModalConfig.defaultFormClass);
+                    if (!cAttrs.name && cAttrs.jdTitle) {
+                        cElement.attr('name', cAttrs.jdTitle);
                     }
-                    if (angular.isUndefined(attrs.novalidate)) {
-                        element.attr('novalidate', '');
+                    if (angular.isUndefined(cAttrs.novalidate)) {
+                        cElement.attr('novalidate', '');
                     }
-                    $timeout(function () {
-                        // seta foco no primeiro elemento do form
-                        jQuery(":input:visible:enabled", element).not('[readonly]').filter(':first').focus();
-                    }, 10);
                 }
 
                 if (ModalConfig.defaultTableClass) {
-                    var table = element.find('table.table');
-                    if (table.length > 0 && !element.hasClass(ModalConfig.defaultTableClass)) {
-                        element.addClass(ModalConfig.defaultTableClass);
+                    var table = cElement.find('table.table');
+                    if (table.length > 0 && !cElement.hasClass(ModalConfig.defaultTableClass)) {
+                        cElement.addClass(ModalConfig.defaultTableClass);
                     }
                 }
 
-                element.parent().find('.modal-header span.close,[jd-dismiss-modal]').on('click', function () {
-                    if (scope.$dismiss) {
-                        scope.$dismiss();
-                    } else
-                        if (scope.$parent.$dismiss) {
-                            scope.$parent.$dismiss();
-                        }
-                    return false;
-                });
-
-                var footer = element.find('.modal-footer');
+                var footer = cElement.find('.modal-footer');
                 if (footer.length > 0) {
-                    element.after(footer);
+                    var f = footer[0].outerHTML;
+                    // remove o footer antes do transclude para adicionar no post link
+                    // footer deve ser "irmão" do body, da forma utilizada, dentro do body, ele precisa ser movido pra fora e recompilado
+                    footer.remove();
+                    footer = f;
                 }
+
+                return function (scope, element) {
+                    if (footer) {
+                        footer = jQuery(footer);
+                        element.append(footer);
+                        $compile(footer)(scope);
+                    }
+
+                    var form = element.find('form:first');
+                    if (form.length > 0) {
+                        $timeout(function () {
+                            // seta foco no primeiro cElemento do form
+                            jQuery(":input:visible:enabled", form).not('[readonly]').filter(':first').focus();
+                        }, 10);
+                    }
+
+                    element.find('[jd-dismiss-modal]').on('click', function () {
+                        scope.$dismiss();
+                        return false;
+                    });
+                };
             }
+        }
+    }]).directive('jdModal', ['jedi.layout.modal.ModalConfig', '$timeout', '$compile', function (ModalConfig, $timeout, $compile) {
+        return {
+            restrict: "A",
+            replace: true,
+            transclude: 'element',
+            priority: 1000,
+            scope: {
+                jdTitle: '@',
+                jdTemplateUrl: '@'
+            },
+            templateUrl: function (elem, attrs) {
+                if (attrs.jdTemplateUrl) {
+                    return attrs.jdTemplateUrl;
+                } else {
+                    return ModalConfig.defaultTemplateUrl;
+                }
+            },
+            controller: ['$scope', '$attrs', '$element', function Controller($scope, $attrs, $element) {
+                $scope.closeModal = function () {
+                    $scope.$parent.$dismiss();
+                    return false;
+                };
+            }]
         }
     }]);
 
