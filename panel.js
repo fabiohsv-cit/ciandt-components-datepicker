d66017f9d53acf2a0e7d6827248e6c9c101eaaeb
Fábio Henrique da Silva Viana <fabiohsv@ciandt.com>
2015-08-12 15:10:57 -0300

    add toggle efect in panel

--- panel.js
+++ panel.js
@@ -11,9 +11,10 @@ define(['ng-jedi-utilities'], function () {
         boxedPageTpl: '<div class="page"></div>',
         bodyTpl: '<section class="panel panel-default"></section>',
         headerTpl: '<div class="panel-heading">' +
-                   '  <strong><span class="glyphicon glyphicon-th"></span><jd-i18n>{{jdTitle}}</jd-i18n></strong>' +
-                   '</div>'
-    }).directive('jdPanel', ['jedi.utilities.Utilities', 'jedi.layout.panel.PanelConfig', '$interpolate', function (utilities, PanelConfig, $interpolate) {
+                   '  <strong>{{jdIcon}}<jd-i18n>{{jdTitle}}</jd-i18n></strong>' +
+                   '</div>',
+        iconTpl: '<span class="glyphicon {{jdTitleIcon}}"></span>'
+    }).directive('jdPanel', ['jedi.utilities.Utilities', 'jedi.layout.panel.PanelConfig', '$interpolate', '$compile', function (utilities, PanelConfig, $interpolate, $compile) {
         return {
             restrict: 'A',
             link: function (scope, element, attrs) {
@@ -32,6 +33,16 @@ define(['ng-jedi-utilities'], function () {
 
                 var wrapper = utilities.wrapElement(element, $interpolate(PanelConfig.bodyTpl)(angular.extend({}, attrs, scope)));
 
+                if (typeof attrs.jdToggle != 'undefined') {
+                    attrs.jdTitleIcon = attrs.jdToggle.toLowerCase().trim() == 'false' ? 'glyphicon-chevron-right' : 'glyphicon-chevron-down';
+                }
+
+                if (!attrs.jdTitleIcon) {
+                    attrs.jdTitleIcon = 'glyphicon-th';
+                }
+
+                attrs.jdIcon = $interpolate(PanelConfig.iconTpl)(angular.extend({}, attrs, scope));
+
                 if (attrs.jdTitle) {
                     wrapper.prepend($interpolate(PanelConfig.headerTpl)(angular.extend({}, attrs, scope)));
                 }
@@ -43,6 +54,30 @@ define(['ng-jedi-utilities'], function () {
                 if (PanelConfig.useBoxedPage && wrapper.parent().is(PanelConfig.containerFilter)) {
                     // cria div page caso o parent seja o container
                     utilities.wrapElement(wrapper, $interpolate(PanelConfig.boxedPageTpl)(angular.extend({}, attrs, scope)));
+                }
+
+                var i18n = element.parents('.panel:first').find('.panel-heading jd-i18n,.panel-heading [jd-i18n]');
+                if (i18n.length > 0) {
+                    $compile(i18n)(scope);
+                }
+
+                if (typeof attrs.jdToggle != 'undefined') {
+                    element.parents('.panel:first').find('.panel-heading *').addClass('toggleable').click(function(e) {
+                        var $target = jQuery(e.target).parents('.panel-heading').find('.glyphicon');
+                        if (element.toggle().is(':visible')) {
+                            $target.removeClass('glyphicon-chevron-right');
+                            $target.addClass('glyphicon-chevron-down');
+                        } else {
+                            $target.removeClass('glyphicon-chevron-down');
+                            $target.addClass('glyphicon-chevron-right');
+                        }
+                        e.stopPropagation();
+                        return false;
+                    });
+
+                    if (attrs.jdToggle.toLowerCase().trim() == 'false') {
+                        element.hide();
+                    }
                 }
             }
         }
